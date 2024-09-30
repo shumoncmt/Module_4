151d82b39ba2299a8ac633248f8b2f99744172a2
shumoncmt <shomoncmt@gmail.com>
2024-09-30 17:53:25 +0600

    aff
    aff

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ $price = 500;
 $quantity = 50;
 $totalPrice = $price * $quantity;
 
+echo "The price of a $item si $quantity";
+echo "<br>";
+print("The Price of a $item is $price");
+echo "<br>";
+printf("The Price of a %s is %d" , $item , $price);
+echo "<br>";
 
 
 
