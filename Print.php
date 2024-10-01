060f20dbcf49aec0060e36048d1d7f036c6cfa60
Md. Shumon Mia <shomoncmt@gmail.com>
2024-10-01 22:23:56 +0600

    add File
    Add File

---
+++ Print.php
@@ -0,0 +1,8 @@
+<?php 
+//Printf Show and Sprintf not show
+$name = "Md . Shumon Mia";
+$age = 27;
+$Male ="True";
+$height =5.4;
+$Rasult= sprintf("My Name is %s %d %s %.2f", $name , $age, $Male, $height);
+echo $Rasult;
