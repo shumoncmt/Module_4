a7f2323d2c3be0fe291ae62807e1097fd9e33aa3
shumoncmt <shomoncmt@gmail.com>
2024-10-05 15:52:02 +0600

    add
     add

---
+++ possative.php
@@ -0,0 +1,11 @@
+<?php
+$number = 00;
+if ($number > 0){
+    echo "This $number is Positive";
+}else if ($number < 0 ){
+     echo " This $number is Negative";
+}else if ($number == 0){
+    echo " This $number is Zero";
+}else{
+    echo "Not Valid Number";
+}
