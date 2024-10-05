3179e0decba7ccd99d63e0f415d0665102afb8c1
shumoncmt <shomoncmt@gmail.com>
2024-10-05 16:10:38 +0600

    add
    Project

---
+++ largebnumber.php
@@ -0,0 +1,11 @@
+<?php 
+$number1 = 100;
+$number2 = 100;
+if ($number1 > $number2){
+    echo " This $number1 is Big Number";
+}else if ($number2 > $number1){
+    echo "This $number2 is Big Number";
+    echo "\n";
+} else{
+    echo " Two Number is Same $number1 == $number2";
+}
