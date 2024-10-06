bf2148e01b551da821b0cd58b9844b9d4179c66e
shumoncmt <shomoncmt@gmail.com>
2024-10-06 10:48:51 +0600

    add
    add

---
+++ grade.php
@@ -0,0 +1,26 @@
+<?php
+$grade = 49;
+switch ($grade) {
+    case ($grade >=80 && $grade<= 100):
+        echo "Grade is A+";
+        break;
+    case ($grade >=70 && $grade < 80):
+        echo " Grade is A";
+        break;
+    case ($grade >= 60 && $grade <70):
+        echo "Grade is A-";
+        break;
+    case ($grade >= 50 && $grade < 60):
+        echo "Grade is B";
+        break;
+    case ($grade >= 40 && $grade < 50):
+        echo "Grade is C";
+        break;
+    case ($grade >= 33 && $grade < 40):
+        echo "Grade is D";
+        break;
+    default:
+    echo "We are Full";
+
+}
+// Grade System In Switch case function 
