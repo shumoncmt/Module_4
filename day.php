45f84d139050139e239f92ee7428ed9ac43463ca
shumoncmt <shomoncmt@gmail.com>
2024-10-06 10:31:05 +0600

    switch case
    switch case

--- day.php
+++ day.php
@@ -1,5 +1,5 @@
 <?php
-$day = 5;
+$day = 10;
 if ($day == 1){
     echo "Saturday";
 } else if( $day == 2){
@@ -17,3 +17,38 @@ if ($day == 1){
 }else{
     echo " Not Mach";
 }
+
+//Switch case
+echo "\n";
+
+switch ($day) {
+    case ($day == 1):
+        echo "Saturday Switch case";
+        break;
+        case ($day == 2):
+            echo "Sunday switch case";
+            break;
+
+        case ($day == 3):
+        echo "Monday Switch Case";
+        break;
+
+        case ($day == 4):
+            echo "Tuesday Switch Case";
+            break;
+
+        case ($day == 5):
+            echo "Wednesday Switch Case";
+             break;
+
+        case ($day ==   6):
+            echo "Thursday Switch Case";
+            break;
+
+        case ($day == 7):
+            echo "Friday Switch Case";
+            break;
+        default:
+        echo "Not Mach Switch Case";
+
+    }
