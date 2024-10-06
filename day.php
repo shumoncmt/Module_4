2e8e12c1f6f3024be7be34584e209855eca3a6c7
shumoncmt <shomoncmt@gmail.com>
2024-10-06 10:12:55 +0600

    aff
    aff

---
+++ day.php
@@ -0,0 +1,19 @@
+<?php
+$day = 5;
+if ($day == 1){
+    echo "Saturday";
+} else if( $day == 2){
+    echo "Sunday";
+} else if( $day == 3){
+    echo "Monday";
+} else if( $day == 4){
+    echo "Tuesday ";
+} else if( $day == 5){
+    echo "Wednesday";
+} else if( $day == 6){
+    echo "Thursday";
+} else if ( $day == 7){
+    echo "Friday";
+}else{
+    echo " Not Mach";
+}
